82e699629d716482875da9d64424a869b9088bbf
Eugene Stucalov <eugene.stuc@gmail.com>
2019-05-02 15:27:26 +0300

    1 commit

---
+++ blog/views.py
@@ -0,0 +1,114 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.views.generic import (ListView, DetailView,
+                                  CreateView, UpdateView,
+                                  DeleteView)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from blog.models import Post
+from .serializers import UserSerializer, GroupSerializer, PostSerializer
+from rest_framework.renderers import JSONRenderer
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+# update view
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    # update viwe
+
+
+# update viwe
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
+
+def get_post(request):
+    resonse = []
+    for obj in Post.objects.all():
+        resonse.append({
+            'id': obj.id,
+            "title": obj.title,
+            "content": obj.content,
+            "author": obj.author,
+            'date_posted': obj.date_posted,
+        })
+
+    return JsonResponse({'response': resonse})
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
