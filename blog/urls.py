ad8b578d8dd8ca8484c4ac6168ba0afcb5b54043
victor0198ebs <victor.ebs0198@gmail.com>
2019-05-03 17:37:59 +0300

    master commit

--- blog/urls.py
+++ blog/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/like/', u_w.PostLikeView, name='post-like'),
     path('post/<int:pk>/dislike/', u_w.PostDislikeView, name='post-dislike'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
